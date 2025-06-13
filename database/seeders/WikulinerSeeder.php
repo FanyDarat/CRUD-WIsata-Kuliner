2c7bd4d3145851dd7f34b1c81bd458c4250b5de1
rafaelabednego123 <rafaelabednego123@gmail.com>
2025-06-13 14:41:39 +0000

    Menambahkan seeder

--- database/seeders/WikulinerSeeder.php
+++ database/seeders/WikulinerSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\wikuliner;
 
 class WikulinerSeeder extends Seeder
 {
@@ -12,6 +13,22 @@ class WikulinerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $wikuliners = [
+            [
+                'id_user' => null,
+                'name' => "Seblak Dower Serdang Baru",
+                'rating' => "5",
+                'imageUrl' => "gambar/dower.jpg",
+            ],
+            [
+                'id_user' => null,
+                'name' => "Mall of Indonesia",
+                'rating' => "4.5",
+                'imageUrl' => "gambar/mall.jpg",
+            ],
+        ];
+        foreach ($wikuliners as $value) {
+            wikuliner::create($value);
+        }
     }
 }
